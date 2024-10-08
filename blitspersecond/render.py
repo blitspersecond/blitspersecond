c670653889bcf01ae74ad3bc8245432d9092ccfa
Kris Holdich <kris@smkd.net>
2024-10-09 08:31:21 +0900

    refactoring

--- blitspersecond/render.py
+++ blitspersecond/render.py
@@ -1,3 +1,4 @@
+import os
 from pyglet.gl import (
     glActiveTexture,
     glBindTexture,
@@ -15,147 +16,72 @@ from pyglet.gl import (
     GL_TEXTURE0,
     GL_TRIANGLES,
 )
-
 from .config import Config
 from pyglet.graphics import Batch, Group
 from pyglet.graphics.shader import Shader, ShaderProgram
 from pyglet.image import Texture
 
 
-class RenderGroup(Group):
-    """A Group that enables and binds a Texture and ShaderProgram.
-
-    RenderGroups are equal if their Texture and ShaderProgram
-    are equal.
-    """
-
-    def __init__(self, texture, program, order=0, parent=None):
-        """Create a RenderGroup.
-
-        :Parameters:
-            `texture` : `~pyglet.image.Texture`
-                Texture to bind.
-            `program` : `~pyglet.graphics.shader.ShaderProgram`
-                ShaderProgram to use.
-            `order` : int
-                Change the order to render above or below other Groups.
-            `parent` : `~pyglet.graphics.Group`
-                Parent group.
-        """
-        super().__init__(order, parent)
-        self.texture = texture
-        self.program = program
-
-    def set_state(self):
-        glActiveTexture(GL_TEXTURE0)
-        glBindTexture(self.texture.target, self.texture.id)
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        self.program.use()
-
-    def unset_state(self):
-        glDisable(GL_BLEND)
-
-    def __hash__(self):
-        return hash(
-            (
-                self.texture.target,
-                self.texture.id,
-                self.order,
-                self.parent,
-                self.program,
-            )
-        )
-
-    def __eq__(self, other):
-        return (
-            self.__class__ is other.__class__
-            and self.texture.target == other.texture.target
-            and self.texture.id == other.texture.id
-            and self.order == other.order
-            and self.program == other.program
-            and self.parent == other.parent
-        )
-
-
-vertex_code = """
-#version 330 core
-in vec2 position;
-in vec2 tex_coords;
-out vec2 TexCoord;
-
-uniform WindowBlock
-{
-    mat4 projection;
-    mat4 view;
-} window;
-
-void main()
-{
-    gl_Position = window.projection * window.view * vec4(position, 0.0, 1.0);
-    TexCoord = vec2(tex_coords.x, 1.0 - tex_coords.y); // Flip the y-coordinate here
-}
-"""
-
-fragment_code = """
-#version 330 core
-in vec2 TexCoord;
-out vec4 FragColor;
-
-uniform sampler2D texture1;
-
-void main()
-{
-    FragColor = texture(texture1, TexCoord); // Use TexCoord directly, no flip here
-}
-"""
-
-
 class Renderer:
     def __init__(self):
         self._c = Config()
-        self._width = self._c.window.width
-        self._height = self._c.window.height
-        self._scale = self._c.window.scale
         self._indices = (0, 1, 2, 0, 2, 3)
-        self._vertices = (
-            0,
-            0,
-            1 * self._width * self._scale,
-            0,
-            1 * self._width * self._scale,
-            1 * self._height * self._scale,
-            0,
-            self._height * self._scale,
-        )
         self._coords = (0, 0, 1, 0, 1, 1, 0, 1)
 
-        vertex_shader = Shader(vertex_code, "vertex")
-        fragment_shader = Shader(fragment_code, "fragment")
+        # Load shaders from external files
+        vertex_shader = self._load_shader("vertex.glsl", "vertex")
+        fragment_shader = self._load_shader("fragment.glsl", "fragment")
         self._program = ShaderProgram(vertex_shader, fragment_shader)
+
+    def _load_shader(self, filename: str, shader_type: str) -> Shader:
+        module_dir = os.path.dirname(__file__)
+        file_path = os.path.join(module_dir, "resources", filename)
+        with open(file_path, "r") as shader_file:
+            return Shader(shader_file.read(), shader_type)
 
     def render(self, texture: Texture):
         batch = Batch()
 
-        texture.height = self._height * self._scale
-        texture.width = self._width * self._scale
+        width = self._c.window.width
+        height = self._c.window.height
+        scale = self._c.window.scale
 
+        # Calculate scaled vertices based on current width, height, and scale
+        # fmt: off
+        vertices = (
+            0, 0,
+            width * scale, 0,
+            width * scale, height * scale,
+            0, height * scale,
+        )
+        # fmt: on
+
+        # Bind texture and set parameters
+        glActiveTexture(GL_TEXTURE0)
         glBindTexture(GL_TEXTURE_2D, texture.id)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
 
-        group = RenderGroup(texture, self._program)
-        group.set_state()
+        # Set up blending and shader program
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        self._program.use()
+
+        # Add the vertex list to the batch, with Group set to None
 
         self._program.vertex_list_indexed(
             4,
             GL_TRIANGLES,
             self._indices,
             batch,
-            group,
-            position=("f", self._vertices),
+            None,
+            position=("f", vertices),
             tex_coords=("f", self._coords),
         )
 
+        # Draw the batch
         batch.draw()
-        group.unset_state()
+
+        # Clean up state
+        glDisable(GL_BLEND)
+        self._program.stop()
